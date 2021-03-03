ffa4f06703a58ac835bef96e0df1f4aca744e6b5
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2021-03-02 20:44:58 -0700

    Remove unncessary poro file. Remove unncessary dashboard controller instance variable

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,6 @@
 class Admin::DashboardController < ApplicationController
 
 	def index
-		@json_commits = GithubService.commits
 		@top_customers = Customer.top_customers
 		@incomplete_invoices = Invoice.incomplete_invoices
 	end
--- app/poro/pr.rb
+++
@@ -1,4 +0,0 @@
-class Pr
-  def initialize(pr_data)
-  end
-end
