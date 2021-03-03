2bc31319afc756427accb52d57db96f758d2c737
Ben Fulton <benjamin.fulton99@gmail.com>
2021-03-03 13:28:35 -0700

    Update authentication to use env variables

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
-	# before_action :application
+	before_action :application
 
-	# def application
-	# 	@commits = GithubService.commits
+	def application
+		@commits = GithubService.commits
 
 
 
-	# 	#@usable object in application.html.erb
-	# end
+		#@usable object in application.html.erb
+	end
 end
