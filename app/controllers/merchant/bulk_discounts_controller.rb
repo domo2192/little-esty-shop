2912e60d462435508d93cdc0ce90c928129f760a
Dominic Padula <domopadula2192@gmail.com>
2021-03-05 10:44:19 -0700

    create and new actions with sad paths for created new bulk discounts

--- app/controllers/merchant/bulk_discounts_controller.rb
+++ app/controllers/merchant/bulk_discounts_controller.rb
@@ -1,17 +1,35 @@
 class Merchant::BulkDiscountsController < ApplicationController
-    before_action :find_merchant, only: [:index, :show]
+    before_action :find_merchant, only: [:index, :show, :new, :create]
 
   def index
-    @json_holidays ||= GithubService.holidays 
+    @json_holidays ||= GithubService.holidays
   end
 
   def show
     @bulk_discount = BulkDiscount.find(params[:id])
   end
 
+  def new
+  end
+
+  def create
+    @bulk_discount = @merchant.bulk_discounts.new(bulk_discounts_create_params)
+    if @bulk_discount.save
+      flash[:notice] = "#{@bulk_discount.name} was created"
+      redirect_to merchant_bulk_discounts_path(@merchant)
+    else
+      flash[:notice] = "Enter Integers for both quantity and percentage"
+      render :new
+    end 
+  end
+
 
   private
   def find_merchant
     @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def bulk_discounts_create_params
+    params.permit(:name, :percentage, :quantity)
   end
 end
