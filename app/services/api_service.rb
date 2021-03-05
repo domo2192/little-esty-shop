93a095c34eba01d688d2774febea060e93f85183
Dominic Padula <domopadula2192@gmail.com>
2021-03-05 09:26:57 -0700

    get api infomration for upcoming holidays

--- app/services/api_service.rb
+++ app/services/api_service.rb
@@ -1,9 +1,15 @@
 require "faraday"
 class ApiService
 	def self.get_data(endpoint)
-		response = Faraday.get(endpoint)do |req|
+		response = Faraday.get(endpoint) do |req|
 			req.headers['Authorization'] = "token #{ENV['GITHUB_TOKEN']}"
 		end
+		data = response.body
+		JSON.parse(data, symbolize_names: true)
+	end
+
+	def self.get_holidays(endpoint)
+		response = Faraday.get(endpoint)
 		data = response.body
 		JSON.parse(data, symbolize_names: true)
 	end
