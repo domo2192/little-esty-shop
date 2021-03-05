dae9a93fb374981bf6d302c7801615a1f10393a5
Dominic Padula <domopadula2192@gmail.com>
2021-03-05 07:54:06 -0700

    add relationships and tests for merchant

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -6,6 +6,7 @@ class Merchant < ApplicationRecord
   has_many :invoices, through: :invoice_items
   has_many :transactions, through: :invoices
   has_many :customers, through: :invoices
+  has_many :discounts 
 
   def top_five_customers
     transactions.joins(invoice: :customer).select("customers.*, COUNT(transactions) AS count_of_success, customers.id AS customer_id").where("transactions.result = ?", 'success').group("customers.id").order("count_of_success DESC").limit(5)
@@ -30,7 +31,7 @@ class Merchant < ApplicationRecord
   def self.top_five_merchants
     InvoiceItem.joins("JOIN invoices ON invoice_items.invoice_id = invoices.id JOIN items ON invoice_items.item_id = items.id JOIN transactions ON transactions.invoice_id = invoices.id JOIN merchants ON merchants.id = items.merchant_id").where("transactions.result = 'success' AND invoices.status != 0").group("merchants.id").select("merchants.id, merchants.name , SUM (invoice_items.quantity * invoice_items.unit_price) AS revenue").order("revenue DESC").limit(5)
   end
-  
+
   def top_five_items
     items.joins(:invoice_items).joins(invoices: :transactions).select('items.*, Sum(invoice_items.quantity * invoice_items.unit_price) AS Revenue').where("transactions.result = ?", 'success').group("items.id").order("Revenue DESC").limit(5)
   end
