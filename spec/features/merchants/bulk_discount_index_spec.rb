6fbf794152ad841734f459c49c876b7a79aaa2ff
Dominic Padula <domopadula2192@gmail.com>
2021-03-05 08:09:48 -0700

    refactor test

--- spec/features/merchants/bulk_discount_index_spec.rb
+++ spec/features/merchants/bulk_discount_index_spec.rb
@@ -3,9 +3,9 @@ require "rails_helper"
 RSpec.describe 'Merchant discount index ' do
   it "shows all the available discounts to a merchant" do
     mer_1 = create(:merchant)
-    bulk_discount1 = mer_1.bulk_discount.create(name:"A", percentage:10.0, quantity: 10)
-    bulk_discount2 = mer_1.bulk_discount.create(name:"B", percentage:5.0, quantity: 5)
-    bulk_discount3 = mer_1.bulk_discount.create(name:"C", percentage:15.0, quantity: 20)
+    bulk_discount1 = create(:bulk_discount, merchant_id:mer_1.id)
+    bulk_discount2 = create(:bulk_discount, merchant_id:mer_1.id)
+    bulk_discount3 = create(:bulk_discount, merchant_id:mer_1.id)
     visit(merchant_dashboard_index_path(mer_1))
     expect(page).to have_link(merchant_discounts_path(mer_1))
   end
