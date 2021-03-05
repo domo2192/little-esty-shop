fd3b27f94a686bd58d3a1fdfca5a0b8a77010cda
Dominic Padula <domopadula2192@gmail.com>
2021-03-05 15:40:33 -0700

    create action that includes sad path

--- app/controllers/merchant/bulk_discounts_controller.rb
+++ app/controllers/merchant/bulk_discounts_controller.rb
@@ -1,6 +1,6 @@
 class Merchant::BulkDiscountsController < ApplicationController
-    before_action :find_merchant, only: [:index, :show, :new, :create, :destroy]
-    before_action :find_discount, only: [:show, :destroy]
+    before_action :find_merchant, only: [:index, :show, :new, :create, :destroy, :edit, :update]
+    before_action :find_discount, only: [:show, :destroy, :edit, :update]
 
   def index
     @json_holidays ||= GithubService.holidays
@@ -23,6 +23,19 @@ class Merchant::BulkDiscountsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @bulk_discount.update(update_bulk_discount_params)
+      flash[:notice] = "#{@bulk_discount.name} was updated"
+      redirect_to merchant_bulk_discount_path(@merchant, @bulk_discount)
+    else
+      flash[:notice] = "Enter Integers for both quantity and percentage and word for name"
+      render :edit
+    end
+  end
+
   def destroy
     BulkDiscount.destroy(@bulk_discount.id)
     redirect_to merchant_bulk_discounts_path(@merchant)
@@ -40,5 +53,9 @@ class Merchant::BulkDiscountsController < ApplicationController
 
   def bulk_discounts_create_params
     params.permit(:name, :percentage, :quantity)
+  end
+
+  def update_bulk_discount_params
+    params.require(:bulk_discount).permit(:name, :percentage, :quantity)
   end
 end
