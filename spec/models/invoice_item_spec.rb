d52080adbed42451b44ea7069be41e9262c05d1f
Dominic Padula <domopadula2192@gmail.com>
2021-03-09 13:50:37 -0700

    testing for models and view for applying discount to revenue

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -19,6 +19,11 @@ describe InvoiceItem do
   describe "relationships" do
     it { should belong_to :invoice }
     it { should belong_to :item }
+  	it { should have_many(:transactions).through(:invoice) }
+  	it { should have_one(:merchant).through(:item) }
+  	it { should have_one(:customer).through(:invoice) }
+  	it { should have_many(:bulk_discounts).through(:merchant) }
+
   end
   describe "class methods" do
   	it "::items_by_invoice" do
@@ -29,4 +34,12 @@ describe InvoiceItem do
   		expect(@invoice_item1.merchants_best_day(@mer_1.id).first.id).to eq(@mer_1.id)
   	end
   end
+
+	it "can find the bulk discount of a single invoice_item" do
+		InvoiceItem.destroy_all
+		invoice_item1 = create(:invoice_item, invoice_id: @invoice1.id, item_id: @item_1.id, quantity: 5, unit_price: 10)
+		bulk_discount1 = create(:bulk_discount, merchant_id: @mer_1.id, percentage: 20, quantity:5)
+		expect(invoice_item1.discounted).to eq(40)
+	end
+
 end
--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -24,10 +24,11 @@ describe Invoice do
         invoice_item1 = create(:invoice_item, item_id:@item_1.id, invoice_id:@invoice1.id, quantity: 8, unit_price: 2, status: "shipped")
         invoice_item2 = create(:invoice_item, item_id:@item_2.id, invoice_id:@invoice1.id, quantity: 10, unit_price: 5, status: 2)
         invoice_item3 = create(:invoice_item, item_id:@item_2.id, invoice_id:@invoice1.id, quantity: 5, unit_price: 2, status: 2)
-        invoice_item4 = create(:invoice_item, item_id:@item_4.id, invoice_id:@invoice4.id, quantity: 3, unit_price: 5, status: 0)
-        invoice_item4 = create(:invoice_item, item_id:@item_5.id, invoice_id:@invoice6.id, quantity: 3, unit_price: 5, status: 0)
-        invoice_item4 = create(:invoice_item, item_id:@item_6.id, invoice_id:@invoice5.id, quantity: 3, unit_price: 5, status: 0)
+        invoice_item4 = create(:invoice_item, item_id:@item_4.id, invoice_id:@invoice2.id, quantity: 3, unit_price: 5, status: 0)
+        invoice_item4 = create(:invoice_item, item_id:@item_5.id, invoice_id:@invoice2.id, quantity: 3, unit_price: 5, status: 0)
+        invoice_item4 = create(:invoice_item, item_id:@item_6.id, invoice_id:@invoice2.id, quantity: 3, unit_price: 5, status: 0)
         transaction1 = create(:transaction, invoice_id: @invoice1.id, result: "success")
+        transaction2 = create(:transaction, invoice_id: @invoice2.id, result: "success")
     end
   describe "relationships" do
     it { should belong_to :customer }
@@ -42,8 +43,42 @@ describe Invoice do
     end
 
     it "#incomplete_invoices" do
-        expect(Invoice.incomplete_invoices.length).to eq(3)
-        expect(Invoice.incomplete_invoices.first.id).to eq(@invoice4.id)
+        expect(Invoice.incomplete_invoices.length).to eq(1)
+        expect(Invoice.incomplete_invoices.first.id).to eq(@invoice2.id)
     end
+  end
+
+  it "can get all revenue of an invoice discounts to revenue" do
+    expect(Invoice.total_revenue(@invoice1.id)).to eq(76)
+    expect(Invoice.total_revenue(@invoice2.id)).to eq(45)
+  end
+
+  it "can discount from total revenue" do
+    bulk_discount1 = create(:bulk_discount, merchant_id: @mer_1.id, percentage: 20, quantity:5)
+    expect(@invoice1.discount).to eq(60.8)
+  end
+
+  it "edgecase testing for finding total revenue" do
+    bulk_discount1 = create(:bulk_discount, merchant_id: @mer_1.id, percentage:10 , quantity:10)
+    expect(@invoice1.discount).to eq(71)
+  end
+
+  it "more testing for total revenue with discounts" do
+    InvoiceItem.destroy_all
+    invoice_item1 = create(:invoice_item, item_id:@item_1.id, invoice_id:@invoice1.id, quantity: 12, unit_price: 1, status: "shipped")
+    invoice_item2 = create(:invoice_item, item_id:@item_2.id, invoice_id:@invoice1.id, quantity: 15, unit_price: 1, status: 2)
+    bulk_discount1 = create(:bulk_discount, merchant_id: @mer_1.id, percentage:20 , quantity:10)
+    bulk_discount2 = create(:bulk_discount, merchant_id: @mer_1.id, percentage:30, quantity:15)
+    expect(@invoice1.discount).to eq(20.1)
+
+  end
+
+  it "testing edgecases for total rev" do
+    InvoiceItem.destroy_all
+    invoice_item1 = create(:invoice_item, item_id:@item_1.id, invoice_id:@invoice1.id, quantity: 10, unit_price: 1, status: "shipped")
+    invoice_item2 = create(:invoice_item, item_id:@item_2.id, invoice_id:@invoice1.id, quantity: 15, unit_price: 1, status: 2)
+    bulk_discount1 = create(:bulk_discount, merchant_id: @mer_1.id, percentage:20 , quantity:10)
+    bulk_discount2 = create(:bulk_discount, merchant_id: @mer_1.id, percentage:15, quantity:15)
+    expect(@invoice1.discount).to eq(20)
   end
 end
